2c608d9ce310a6774cd830bdf33f2d2a162ab6c7
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-03 16:16:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,10 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-  validates :artist_name, presence: true 
+  validates :artist_name, presence: true
   validates :genre, presence: true 
   validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released?, numericality: { less_than_or_equal_to: :current_year }
-  validates :title, :release_year, uniqueness: true 
+  validates :title, :release_year, uniqueness: true
 
   def released?
     released
