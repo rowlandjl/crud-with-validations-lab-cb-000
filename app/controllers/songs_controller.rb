def6097527fce92a35894b22a445d37cac54d0e3
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-03 17:12:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
     @song = Song.new(song_params)
 
     if @song.save
-      redirect_to song_path(song_params)
+      redirect_to song_path(@song)
     else
       render :new
     end
