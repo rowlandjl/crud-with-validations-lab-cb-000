8a0d7ff11e729f13b0a470bf25fc3ab6fc4bd135
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-03 15:48:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,21 @@
 class SongsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def udpate
+  end
+
+  def destroy
+  end
+  
 end
