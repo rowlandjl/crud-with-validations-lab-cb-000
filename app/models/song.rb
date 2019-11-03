04c646b3991259b1ab96ad559df5bb2cc95f091e
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-03 15:56:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
   validates :title, presence:true
   validates :released, inclusion: { in: %w(true false) }
+  validates :release_year, numericality: { only_integer: true }
 end
