16f0f9444050b308838c661502cd610151ee8b56
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-03 15:38:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_songs.rb
+++ db/migrate/001_create_songs.rb
@@ -2,10 +2,10 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      t.boolean :release
+      t.boolean :released
       t.integer :release_year
       t.string :artist_name
       t.string :genre
-    end 
+    end
   end
 end
